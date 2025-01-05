6015ce0336b2628c14d6d8d7d22e8444ae3ba09a
Serkan Okumuş <serkanokumus@gmail.com>
2025-01-05 21:38:03 +0300

    Delete Insider - Automated Test directory

--- Insider - Automated Test/src/main/java/utilities/EnvironmentManager.java
+++
@@ -1,9 +0,0 @@
-package utilities;
-
-public class EnvironmentManager {
-    private static final String BASE_URL = "https://useinsider.com";
-
-    public static String getBaseUrl() {
-        return BASE_URL;
-    }
-}
